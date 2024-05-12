9606ab988955250693ebd73d54efcbf55cd1bff7
bence-lazar <79217196+bence-lazar@users.noreply.github.com>
2024-05-12 12:06:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,16 +6,16 @@ import streamlit as st
 """
 # Welcome to Streamlit!
 
-st.divider()
-st.divider()
-st.divider()
-
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
+
+st.divider()
+st.divider()
+st.divider()
 
 num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
 num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
